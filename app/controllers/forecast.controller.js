b21616e8239201cb4a4e309ac4bc827923d297e7
haworku <haworku@gmail.com>
2017-03-03 17:29:37 -0600

    getForecast method calculates start and end using moment(untested)

--- app/controllers/forecast.controller.js
+++ app/controllers/forecast.controller.js
@@ -2,9 +2,27 @@
   angular.module('theApp')
     .controller('ForecastController', ForecastController);
 
-  ForecastController.$inject = ['$location'];
+  ForecastController.$inject = ['$scope', '$location', 'moment'];
 
-  function ForecastController($location) {
+  function ForecastController($scope, $location, moment) {
     let vm = this;
+    vm.forecastDuration = moment.duration(2, 'd')
+    vm.getForecast = () => {
+    	let start = moment($rootScope.today).format('YYYY-MM-DD')
+    	let endUnformatted =  vm.today.add(vm.forecastDuration);
+    	let end = moment(endUnformatted).format('YYYY-MM-DD')
+          
+    api.get(zip, start, end)
+    .then((response) => { 
+      vm.weatherObj = vm.processData(response.results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });  
+
+    }
+ 	 
+
+
 }
 })(window.angular);
