5ed32cfaae0f67c06c4268998a47b572da45d461
haworku <haworku@gmail.com>
2017-03-03 17:30:12 -0600

    processData reorganizes response for easy display (Tested!)

--- app/controllers/forecast.controller.js
+++ app/controllers/forecast.controller.js
@@ -7,6 +7,27 @@
   function ForecastController($scope, $location, moment) {
     let vm = this;
     vm.forecastDuration = moment.duration(2, 'd')
+    vm.weatherObj = {}; // data from various stations, nested by date and then by datatype 
+
+    vm.processData = (data) => {
+    	let nestedData = {}
+
+    	data.forEach( result => {
+    		if ( result.date in nestedData === false) { 
+    			nestedData[result.date] = {};
+    		}
+
+    		if ( result.dataType in nestedData[result.date] == false) {
+    			// console.log(nestedData[result.date]);
+    			nestedData[result.date][result.datatype] = result.value;
+    		} else {
+    			nestedData[result.date][result.datatype] += result.value;
+    		}
+    	})
+
+    	return nestedData
+    }
+
     vm.getForecast = () => {
     	let start = moment($rootScope.today).format('YYYY-MM-DD')
     	let endUnformatted =  vm.today.add(vm.forecastDuration);
