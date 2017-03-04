c0154860328d77eb0feee57857aa3259347b6bcf
haworku <haworku@gmail.com>
2017-03-03 23:59:11 -0600

    round temps to one decimal

--- app/controllers/forecast.controller.js
+++ app/controllers/forecast.controller.js
@@ -13,47 +13,47 @@
     		now:  { 
     			summary: current.weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ current.weather[0].icon }.png`,
-					temp: current.main.temp,
+					temp: current.main.temp.toFixed(1),
 				},
 				today: {
 					date: moment(),
     			summary: forecast[0].weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ forecast[0].weather[0].icon }.png`,
-    			temp: forecast[0].temp.day,
-					max: forecast[0].temp.max,
-					min: forecast[0].temp.min
+    			temp: forecast[0].temp.day.toFixed(1),
+					max: forecast[0].temp.max.toFixed(1),
+					min: forecast[0].temp.min.toFixed(1)
 				},
 				one:  { 
 					date: moment().add(1, 'days'),
     			summary: forecast[1].weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ forecast[0].weather[0].icon }.png`,
-    			temp: forecast[1].temp.day,
-					max: forecast[1].temp.max,
-					min: forecast[1].temp.min
+    			temp: forecast[1].temp.day.toFixed(1),
+					max: forecast[1].temp.max.toFixed(1),
+					min: forecast[1].temp.min.toFixed(1)
 				},
 				two:  { 
 					date: moment().add(2, 'days'),
     			summary: forecast[2].weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ forecast[0].weather[0].icon }.png`,
-    			temp: forecast[2].temp.day,
-					max: forecast[2].temp.max,
-					min: forecast[2].temp.min
+    			temp: forecast[2].temp.day.toFixed(1),
+					max: forecast[2].temp.max.toFixed(1),
+					min: forecast[2].temp.min.toFixed(1)
 				},
 				three:  { 
 					date: moment().add(3, 'days'),
     			summary: forecast[3].weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ forecast[0].weather[0].icon }.png`,
-    			temp: forecast[3].temp.day,
-					max: forecast[3].temp.max,
-					min: forecast[3].temp.min
+    			temp: forecast[3].temp.day.toFixed(1),
+					max: forecast[3].temp.max.toFixed(1),
+					min: forecast[3].temp.min.toFixed(1)
 				},
 				four:  { 
 					date: moment().add(4, 'days'),
     			summary: forecast[4].weather[0].description,
     			icon: `http://openweathermap.org/img/w/${ forecast[0].weather[0].icon }.png`,
-    			temp: forecast[4].temp.day,
-					max: forecast[4].temp.max,
-					min: forecast[4].temp.min
+    			temp: forecast[4].temp.day.toFixed(1),
+					max: forecast[4].temp.max.toFixed(1),
+					min: forecast[4].temp.min.toFixed(1)
 
 				},
 				five:  { 
